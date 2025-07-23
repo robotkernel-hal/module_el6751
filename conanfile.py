a963e145941759afc76b69783250e4544334ce22
Robert Burger <robert.burger@dlr.de>
2025-07-23 08:19:27 +0200

    change: configure.ac generation

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,9 @@ class MainProject(ConanFile):
     url = "https://rmc-github.robotic.dlr.de/robotkernel/module_el6751.git"
     description = ""
     exports_sources = ["*", "!.gitignore"]
+    
+    def source(self):
+        self.run(f"sed 's|PACKAGE_VERSION|{self.version}|' configure.ac.in > configure.ac")
 
     def requirements(self):
         self.requires("robotkernel/[~6]@robotkernel/unstable")
