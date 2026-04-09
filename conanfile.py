645014b32b965d3c384715a48a92e34142b52abb
Robert Burger <robert.burger@dlr.de>
2026-04-09 06:47:06 +0200

    fix: conan template file

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,13 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~5]@robotkernel/stable"
+    python_requires = "conan_template/[~6]@robotkernel/unstable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_el6751"
     url = "https://rmc-github.robotic.dlr.de/robotkernel/module_el6751.git"
     description = ""
     exports_sources = ["*", "!.gitignore"]
-    
-    def source(self):
-        self.run(f"sed 's|PACKAGE_VERSION|{self.version}|' configure.ac.in > configure.ac")
 
     def requirements(self):
         self.requires("robotkernel/[~6]@robotkernel/unstable")
