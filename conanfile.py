13e0c98899b3e28ed7eb45e38b40e463e7e2962e
Robert Burger <robert.burger@dlr.de>
2026-05-26 06:22:58 +0200

    change: using stable deps

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~6]@robotkernel/unstable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_el6751"
@@ -10,4 +10,4 @@ class MainProject(ConanFile):
     exports_sources = ["*", "!.gitignore"]
 
     def requirements(self):
-        self.requires("robotkernel/[~6]@robotkernel/unstable")
+        self.requires("robotkernel/[~6]@robotkernel/stable")
